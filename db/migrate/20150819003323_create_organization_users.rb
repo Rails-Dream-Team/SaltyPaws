696a9401e8ed671020e0099fdb1800ad230a9ef2
Jamie Simper <fromraintohope@gmail.com>
2015-08-18 20:57:21 -0400

    Org-User JT created, with tests and model updates

---
+++ db/migrate/20150819003323_create_organization_users.rb
@@ -0,0 +1,12 @@
+class CreateOrganizationUsers < ActiveRecord::Migration
+  def change
+    create_table :organization_users do |t|
+      t.integer  "user_id"
+      t.integer  "organization_id"
+      t.boolean  "admin"
+
+      t.timestamps null: false
+    end
+    add_index :organization_users, [:user_id, :organization_id], unique: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150818202320) do
+ActiveRecord::Schema.define(version: 20150819003323) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,16 @@ ActiveRecord::Schema.define(version: 20150818202320) do
   add_index "friendly_id_slugs", ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type", using: :btree
   add_index "friendly_id_slugs", ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id", using: :btree
   add_index "friendly_id_slugs", ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type", using: :btree
+
+  create_table "organization_users", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "organization_id"
+    t.boolean  "admin"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
+  add_index "organization_users", ["user_id", "organization_id"], name: "index_organization_users_on_user_id_and_organization_id", unique: true, using: :btree
 
   create_table "organizations", force: :cascade do |t|
     t.string   "name",           null: false
