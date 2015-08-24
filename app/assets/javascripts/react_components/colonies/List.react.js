18df08f0647acb8ad37715e529139ab0f5622321
Jamie Simper <fromraintohope@gmail.com>
2015-08-24 10:51:45 -0400

    functionality working, but needs reorganizing

---
+++ app/assets/javascripts/react_components/colonies/List.react.js
@@ -0,0 +1,26 @@
+var React = require('react');
+var Colony = require('./Colony.react.js');
+
+var List = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <div class="colony_header">
+          <h1>Colonies</h1>
+          <a href="/colonies/new">Add A New Colony</a>
+        </div>
+        <div className="colonies_list">
+        {this._buildItems()}
+        </div>
+      </div>
+    );
+  },
+
+  _buildItems: function() {
+    return this.props.colonies.map(function(item) {
+      return <Colony data={item} updateColony={this.props.updateColony} removeColony={this.props.removeColony} key={item.name + item.id} />
+    }.bind(this));
+  }
+});
+
+module.exports = List;
