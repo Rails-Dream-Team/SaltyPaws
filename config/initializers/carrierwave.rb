cdab0b433eccc06f9dc4c8cf1c8c6ed2d4c1bb9d
Elizabeth Smith <liz.allen.smith@gmail.com>
2015-08-25 16:35:24 -0400

    fetch the ENV variable

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,8 +2,8 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'
   config.fog_credentials = {
     provider:              'AWS',
-    aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
-    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+    aws_access_key_id:     ENV.fetch('AWS_ACCESS_KEY_ID') { 'abc' },
+    aws_secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY') { '123' }
   }
 
   config.fog_directory  = 'salty-avatar'
