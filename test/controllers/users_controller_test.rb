dbef8367431b11f2d6f9e769c75cee86981bc85a
Elizabeth Smith <liz.allen.smith@gmail.com>
2015-08-28 09:25:37 -0400

    tests for user controller and updated user show json

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,28 +2,61 @@ require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
   def setup
-    @user = users(:one)
-    sign_in @user
+    @user1 = users(:one)
+    sign_in @user1
+    @user2 = users(:two)
+    @attributes = ['id', 'first_name', 'last_name', 'display_name', 'email']
+  end
+
+  test "GET show with html for current user profile" do
+    get :show, id: @user1
+    assert_equal @user1, assigns(:user)
+    assert_response :success
+  end
+
+  test "GET show with json for current user profile" do
+    get :show, id: @user1, format: :json
+    response_item = JSON.parse(response.body)
+    @attributes.each do |a|
+      assert_equal @user1.send(a), response_item[a]
+    end
+    assert_response :success
+  end
+
+  test "GET show with html for not-current user profile" do
+    get :show, id: @user1
+    assert_equal @user1, assigns(:user)
+    assert_response :success
+  end
+
+  test "GET show with json for not-current user profile" do
+    get :show, id: @user1
+    assert_equal @user1, assigns(:user)
+    assert_response :success
   end
 
   test "GET edit with html" do
-    get :edit, id: @user
-    assert_equal @user, assigns(:user)
+    get :edit, id: @user1
+    assert_equal @user1, assigns(:user)
     assert_response :success
   end
 
   test "PATCH update with html" do
-    old_name = @user.display_name
+    old_name = @user1.display_name
     new_name = "New User1"
-    patch :update, id: @user, user: { display_name: new_name }
-    @user.reload
-    refute @user.display_name == old_name
-    assert_redirected_to user_path(@user)
+    patch :update, id: @user1, user: { display_name: new_name }
+    @user1.reload
+    refute @user1.display_name == old_name
+    assert_redirected_to user_path(@user1)
   end
 
   test "PATCH update with json" do
-    patch :update, format: :json, id: @user, user: { display_name: "New User" }
+    old_name = @user1.display_name
+    new_name = "New User1"
+    patch :update, format: :json, id: @user1, user: { display_name: new_name }
+    @user1.reload
     assert_response :success
+    assert_equal new_name, @user1.display_name
   end
 
 end
