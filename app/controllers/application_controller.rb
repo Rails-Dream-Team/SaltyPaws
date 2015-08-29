d91ce4ad052d1476abba54f8419f0c4693d9e027
Elizabeth Smith <liz.allen.smith@gmail.com>
2015-08-29 09:44:43 -0400

    restrict admin dash access, add link to nav

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,13 +3,28 @@ class ApplicationController < ActionController::Base
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
+
+    def authenticate_admin_user!
+      authenticate_user!
+      unless current_user.admin
+        flash[:alert] = "This area is restricted to administrators only."
+        redirect_to root_path
+      end
+    end
+
+    def current_admin_user
+      return nil if user_signed_in? && !current_user.admin
+      current_user
+    end
+
+
   private
 
   def user_not_authorized
     flash[:alert] = "You are not authorized to perform this action."
     redirect_to(request.referrer || root_path)
   end
-  
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
