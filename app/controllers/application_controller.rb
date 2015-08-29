60640913814f9d7cb825586c042503269ef554aa
Elizabeth Smith <liz.allen.smith@gmail.com>
2015-08-29 17:16:08 -0400

    fix bug #102310692, redirect to login upon session destroy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,6 +23,10 @@ class ApplicationController < ActionController::Base
     redirect_to(request.referrer || root_path)
   end
 
+  def after_sign_out_path_for(resource_or_scope)
+    new_user_session_path
+  end
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
