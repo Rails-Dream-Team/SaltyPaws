a2390250f8062e81bd2ca296cabfcebc4e30e20a
Jamie Simper <fromraintohope@gmail.com>
2015-08-28 23:40:36 -0400

    colony policies started

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   include Pundit
 
+  # after_action :verify_authorized, unless: :devise_controller?
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
   def authenticate_admin_user!
--- app/models/colony.rb
+++ app/models/colony.rb
@@ -1,6 +1,6 @@
 class Colony < ActiveRecord::Base
   acts_as_paranoid
-  
+
   has_many :colony_users
   has_many :users, through: :colony_users
   has_many :cats
