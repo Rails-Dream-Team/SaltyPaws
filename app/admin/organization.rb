98e0019b0cffa64147bb98f0dd7a3fd19588c371
Elizabeth Smith <liz.allen.smith@gmail.com>
2015-08-29 11:05:32 -0400

    permit changes to all the things

--- app/admin/organization.rb
+++ app/admin/organization.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Organization do
+  permit_params Organization.column_names
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
