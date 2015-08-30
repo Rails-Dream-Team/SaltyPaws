168c679a5316cb3b298783a537e5e11bc38bfbf4
Elizabeth Smith <liz.allen.smith@gmail.com>
2015-08-29 22:41:54 -0400

    refactor some msg board views, starting to customize AA msg board views

--- test/controllers/boards_controller_test.rb
+++ test/controllers/boards_controller_test.rb
@@ -120,7 +120,7 @@ class BoardsControllerTest < ActionController::TestCase
 
     test 'renders edit with invalid attribute submission (no title)' do
       old_title = @board.title
-      patch :update, id: @board, board: { title: nil }
+      patch :update, id: @board, board: { title: '' }
       @board.reload
       assert_template :edit
       assert_equal old_title, @board.title
