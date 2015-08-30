1dfb149b2330d35e1e9fca0a7afcdc734be685a6
Elizabeth Smith <liz.allen.smith@gmail.com>
2015-08-30 18:22:27 -0400

    wrapping up the topics controller tests for tiered auth

--- test/controllers/topics_controller_test.rb
+++ test/controllers/topics_controller_test.rb
@@ -113,6 +113,162 @@ class TopicsControllerTest < ActionController::TestCase
     end
   end # end of TopicsWhenBasic
 
+  class TopicsCreate < TopicsControllerTest
+    def setup
+      super
+      sign_in @admin
+    end
+
+    test 'admin creates with valid attributes and redirects' do
+      assert_difference('Topic.count', 1) do
+        post :create, topic: { title: 'fake topic, being all fake and whatnot', board_id: @board }
+      end
+      assert_redirected_to topic_path(assigns(:topic))
+    end
+
+    test 'admin renders new with invalid attribute submission (no title)' do
+      assert_no_difference('Topic.count') do
+        post :create, topic: { title: '', board_id: @board }
+      end
+      assert_template :new
+    end
+
+    test 'admin renders new with invalid attribute submission (no board)' do
+      assert_no_difference('Topic.count') do
+        post :create, topic: { title: 'fake topic, being all fake and whatnot', board_id: nil }
+      end
+      assert_template :new
+    end
+
+    test 'volunteer creates with valid attributes and redirects' do
+      sign_in @volunteer
+      assert_difference('Topic.count', 1) do
+        post :create, topic: { title: 'fake topic, being all fake and whatnot', board_id: @board }
+      end
+      assert_redirected_to topic_path(assigns(:topic))
+    end
+
+    test 'basic creates with valid attributes and redirects' do
+      sign_in @basic
+      assert_difference('Topic.count', 1) do
+        post :create, topic: { title: 'fake topic, being all fake and whatnot', board_id: @board }
+      end
+      assert_redirected_to topic_path(assigns(:topic))
+    end
+  end # end of TopicsCreate
+
+  class TopicsUpdate < TopicsControllerTest
+    def setup
+      super
+      sign_in @admin
+    end
+
+    test 'admin updates with valid attributes and redirects' do
+      old_title = @topic1.title
+      new_title = 'fake title, now with more fake'
+      patch :update, id: @topic1, topic: { title: new_title }
+      @topic1.reload
+      assert_redirected_to topic_path(assigns(:topic))
+      assert_equal new_title, @topic1.title
+    end
+
+    test 'admin renders edit with invalid attribute submission (no title)' do
+      old_title = @topic1.title
+      patch :update, id: @topic1, topic: { title: nil }
+      @topic1.reload
+      assert_equal old_title, @topic1.title
+      assert_template :edit
+    end
+
+    test 'admin renders edit with invalid attribute submission (no board)' do
+      old_board = @topic1.board
+      patch :update, id: @topic1, topic: { board_id: nil }
+      @topic1.reload
+      assert_equal old_board, @topic1.board
+      assert_template :edit
+    end
+
+    test 'volunteer updates with valid attr if created by current user' do
+      sign_in @volunteer
+      assert_equal @volunteer, @topic2.user
+      old_title = @topic2.title
+      new_title = 'fake title, now with more fake'
+      patch :update, id: @topic2, topic: { title: new_title }
+      @topic2.reload
+      assert_redirected_to topic_path(assigns(:topic))
+      assert_equal new_title, @topic2.title
+    end
+
+    test 'volunteer redirects if created by different user' do
+      sign_in @volunteer
+      refute @volunteer == @topic1.user
+      old_title = @topic1.title
+      new_title = 'fake title, now with more fake'
+      patch :update, id: @topic1, topic: { title: new_title }
+      @topic1.reload
+      assert_equal old_title, @topic1.title
+      assert_redirected_to(request.referrer || root_path)
+    end
+
+    test 'basic updates with valid attr if created by current user' do
+      sign_in @basic
+      assert_equal @basic, @topic3.user
+      old_title = @topic3.title
+      new_title = 'fake title, now with more fake'
+      patch :update, id: @topic3, topic: { title: new_title }
+      @topic3.reload
+      assert_redirected_to topic_path(assigns(:topic))
+      assert_equal new_title, @topic3.title
+    end
+
+    test 'basic redirects if created by different user' do
+      sign_in @basic
+      refute @basic == @topic1.user
+      old_title = @topic1.title
+      new_title = 'fake title, now with more fake'
+      patch :update, id: @topic1, topic: { title: new_title }
+      @topic1.reload
+      assert_equal old_title, @topic1.title
+      assert_redirected_to(request.referrer || root_path)
+    end
+  end # end of TopicsUpdate
+
+  class TopicsDelete < TopicsControllerTest
+    test 'admin soft deletes and redirects' do
+      sign_in @admin
+      old_unscoped = Topic.unscoped.count
+      assert_difference('Topic.count', -1) do
+        delete :destroy, id: @topic1
+      end
+      @topic1.reload
+      assert_redirected_to topics_path
+      assert_equal old_unscoped, Topic.unscoped.count
+      refute @topic1.deleted_at.nil?
+    end
+
+    test 'volunteer redirects and does not delete' do
+      sign_in @volunteer
+      old_deleted_at = @topic1.deleted_at
+      assert_no_difference('Topic.count') do
+        delete :destroy, id: @topic1
+      end
+      @topic1.reload
+      assert_equal old_deleted_at, @topic1.deleted_at, 'volunteer should not delete'
+      assert_redirected_to(request.referrer || root_path)
+    end
+
+    test 'basic redirects and does not delete' do
+      sign_in @basic
+      old_deleted_at = @topic1.deleted_at
+      assert_no_difference('Topic.count') do
+        delete :destroy, id: @topic1
+      end
+      @topic1.reload
+      assert_equal old_deleted_at, @topic1.deleted_at, 'basic should not delete'
+      assert_redirected_to(request.referrer || root_path)
+    end
+  end # end of TopicsDelete
+
   class TopicsWhenLoggedOut < TopicsControllerTest
     test "get index redirects" do
       get :index
@@ -133,127 +289,23 @@ class TopicsControllerTest < ActionController::TestCase
       get :show, id: @topic1.id
       assert_redirected_to new_user_session_path
     end
-  end # end of TopicsWhenLoggedOut
 
-  class TopicsCreate < TopicsControllerTest
-    def setup
-      super
-      sign_in @admin
-    end
-
-    test 'creates with valid attributes and redirects' do
-      assert_difference('Topic.count', 1) do
-        post :create, topic: { title: 'fake topic, being all fake and whatnot', board_id: @board }
-      end
-      assert_redirected_to topic_path(assigns(:topic))
-    end
-
-    test 'renders new with invalid attribute submission (no title)' do
-      assert_no_difference('Topic.count') do
-        post :create, topic: { title: '', board_id: @board }
-      end
-      assert_template :new
-    end
-
-    test 'renders new with invalid attribute submission (no board)' do
-      assert_no_difference('Topic.count') do
-        post :create, topic: { title: 'fake topic, being all fake and whatnot', board_id: nil }
-      end
-      assert_template :new
-    end
-
-    test 'redirects to login when no user logged in' do
-      sign_out @admin
+    test 'post create redirects' do
       assert_no_difference('Topic.count') do
         post :create, topic: { title: 'fake topic, being all fake and whatnot' }
       end
       assert_redirected_to new_user_session_path
     end
-  end # end of TopicsCreate
 
-  class TopicsUpdate < TopicsControllerTest
-    def setup
-      super
-      sign_in @admin
-    end
-
-    test 'updates with valid attributes and redirects' do
-      old_title = @topic1.title
-      new_title = 'fake title, now with more fake'
-      patch :update, id: @topic1, topic: { title: new_title }
-      @topic1.reload
-      assert_redirected_to topic_path(assigns(:topic))
-      assert_equal new_title, @topic1.title
-    end
-
-    test 'renders edit with invalid attribute submission (no title)' do
-      old_title = @topic1.title
-      patch :update, id: @topic1, topic: { title: nil }
-      @topic1.reload
-      assert_equal old_title, @topic1.title
-      assert_template :edit
-    end
-
-    test 'renders edit with invalid attribute submission (no board)' do
-      old_board = @topic1.board
-      patch :update, id: @topic1, topic: { board_id: nil }
-      @topic1.reload
-      assert_equal old_board, @topic1.board
-      assert_template :edit
-    end
-
-    test 'redirects to login when no user logged in' do
+    test 'patch update redirects' do
       sign_out @admin
       old_title = @topic1.title
       patch :update, id: @topic1, topic: { title: 'fake topic, being all fake and whatnot' }
       assert_redirected_to new_user_session_path
       assert_equal old_title, @topic1.title
     end
-  end # end of TopicsUpdate
 
-  class TopicsDelete < TopicsControllerTest
-    def setup
-      super
-      sign_in @admin
-    end
-
-    test 'soft deletes and redirects to topics when logged in' do
-      old_unscoped = Topic.unscoped.count
-      assert_difference('Topic.count', -1) do
-        delete :destroy, id: @topic1
-      end
-      @topic1.reload
-      assert_redirected_to topics_path
-      assert_equal old_unscoped, Topic.unscoped.count
-      refute @topic1.deleted_at.nil?
-    end
-
-    test 'redirects when volunteer user logged in' do
-      sign_out @admin
-      sign_in @volunteer
-      old_deleted_at = @topic1.deleted_at
-      assert_no_difference('Topic.count') do
-        delete :destroy, id: @topic1
-      end
-      @topic1.reload
-      assert_equal old_deleted_at, @topic1.deleted_at, 'volunteer should not delete'
-      assert_redirected_to(request.referrer || root_path)
-    end
-
-    test 'redirects when basic user logged in' do
-      sign_out @admin
-      sign_in @basic
-      old_deleted_at = @topic1.deleted_at
-      assert_no_difference('Topic.count') do
-        delete :destroy, id: @topic1
-      end
-      @topic1.reload
-      assert_equal old_deleted_at, @topic1.deleted_at, 'basic should not delete'
-      assert_redirected_to(request.referrer || root_path)
-    end
-
-    test 'redirects to login when logged out' do
-      sign_out @admin
+    test 'delete destroy redirects' do
       old_deleted_at = @topic1.deleted_at
       assert_no_difference('Topic.count') do
         delete :destroy, id: @topic1
@@ -262,5 +314,5 @@ class TopicsControllerTest < ActionController::TestCase
       assert_redirected_to new_user_session_path
       assert_equal old_deleted_at, @topic1.deleted_at
     end
-  end # end of TopicsDelete
+  end # end of TopicsWhenLoggedOut
 end
