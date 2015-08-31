ab053dc2eca01b99c078b2117b2853cdea8c472c
Elizabeth Smith <liz.allen.smith@gmail.com>
2015-08-30 20:40:02 -0400

    changed default path upon sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,6 +24,14 @@ class ApplicationController < ActionController::Base
     redirect_to(request.referrer || root_path)
   end
 
+  def after_sign_in_path_for(resource)
+    stored_location_for(resource) || request.referrer || root_path
+  end
+
+  def after_sign_up_path_for(resource)
+    signed_in_root_path(resource)
+  end
+
   def after_sign_out_path_for(resource_or_scope)
     new_user_session_path
   end
